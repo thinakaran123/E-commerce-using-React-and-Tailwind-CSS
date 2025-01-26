c2684fbb11a6f24db5610d7b3ee1990abb990092
Thinakaran Senthilkumar <thina@Thinakarans-MacBook-Pro.local>
2025-01-26 15:39:22 -0500

    Initial project upload

---
+++ shopping-website/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="bg-red-500">Ecommerce</h1>
+    </div>
+  );
+}
+
+export default App;
